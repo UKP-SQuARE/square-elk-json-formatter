e978326c9a989faf1fcf18892abba1819d86af05
Tim Baumgärtner <baumgaertner.t@gmail.com>
2021-11-08 11:15:28 +0100

    inital commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="elk_json_formatter",
+    version="0.0.2",
+    description="",
+    url="www.informatik.tu-darmstadt.de/ukp",
+    author="UKP",
+    author_email="baumgaertner@ukp.informatik.tu-darmstadt.de",
+    packages=['elk_json_formatter'],
+    install_requires=["python-json-logger==2.0.1"]
+)
