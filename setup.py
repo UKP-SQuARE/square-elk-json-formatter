ea5ac3b80bc0f6aa4c718a33346653614ce357bd
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-10-26 09:30:30 +0200

    update to version 0.0.3

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+__version__ = "0.0.3"
+
 setup(
-    name="elk_json_formatter",
-    version="0.0.2",
+    name="square_elk_json_formatter",
+    version=__version__,
+    license="MIT",
     description="",
-    url="www.informatik.tu-darmstadt.de/ukp",
+    url="https://github.com/UKP-SQuARE/square-elk-json-formatter",
+    download_url=f"https://github.com/UKP-SQuARE/square-elk-json-formatter/archive/refs/tags/v{__version__}.tar.gz",
     author="UKP",
     author_email="baumgaertner@ukp.informatik.tu-darmstadt.de",
     packages=find_packages(),
